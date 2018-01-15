c1cfabbc16f6a7585461914bd18cfe7a6f6b31fd
Chris Devereux <devereux.chris@gmail.com>
2018-01-15 14:20:18 +0000

    inf: Add geoip detection to gateway

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("babel-polyfill");
 
 const express = require("express");
+const geoip = require("geoip-lite");
 const { createBackend } = require("./modules/api");
 const { frontend, render } = require("./modules/frontend");
 
@@ -9,6 +10,22 @@ const { PORT = 3000 } = process.env;
 
 frontend.prepare().then(() => {
   const server = express();
+  server.enable("trust proxy");
+
+  server.use((req, res, next) => {
+    const geo = geoip.lookup(req.ip);
+
+    if (geo && geo.ll) {
+      const [latitude, longitude] = geo.ll;
+      req.geolocation = { latitude, longitude };
+    }
+
+    next();
+  });
+
+  server.get("/", (req, res) => {
+    res.redirect("/feed", 301);
+  });
 
   server.use(createBackend());
   server.get("*", render);
