4354f820d03638d24c47b970a1b227920438210c
Chris Devereux <devereux.chris@gmail.com>
2018-01-15 15:35:26 +0000

    (frontend): Add some content to /event page

--- modules/frontend/pages/event.js
+++ modules/frontend/pages/event.js
@@ -1,11 +1,102 @@
 import * as React from "react";
+import { graphql } from "react-relay";
 import PropTypes from "prop-types";
+import { format } from "date-fns";
+import Typography from "material-ui/Typography/Typography";
+import withStyles from "material-ui/styles/withStyles";
+import Card from "material-ui/Card/Card";
+import CardContent from "material-ui/Card/CardContent";
+import { compose } from "recompose";
+import withData from "../lib/withData";
 import PageWrapper from "../components/Page";
 
-export default function EventPage({ url }) {
-  return <PageWrapper>{url.query.id}</PageWrapper>;
+const styles = theme => ({
+  paragraph: {
+    marginBottom: theme.spacing.unit
+  },
+  heading: {
+    marginBottom: theme.spacing.unit
+  },
+  section: {
+    marginTop: theme.spacing.unit * 2
+  }
+});
+
+function EventPage({ event, classes }) {
+  return (
+    <PageWrapper>
+      <Card>
+        <CardContent>
+          <Typography
+            className={classes.heading}
+            type="headline"
+            component="h1"
+          >
+            {event.title}
+          </Typography>
+          <Typography className={classes.heading} type="body1" component="h2">
+            Organised by <strong>{event.organiser}</strong>
+          </Typography>
+          <Typography className={classes.heading} type="body1" component="h3">
+            {getTimings(event)}
+          </Typography>
+          <Typography className={classes.heading} type="body1" component="p">
+            {event.address.split(`\n`).map(line => (
+              <span>
+                {line}
+                <br />
+              </span>
+            ))}
+            {event.postcode}
+          </Typography>
+          <section className={classes.section}>
+            {event.longDescription.split("\n").map(paragraph => (
+              <Typography
+                className={classes.paragraph}
+                type="body2"
+                component="p"
+              >
+                {paragraph}
+              </Typography>
+            ))}
+          </section>
+        </CardContent>
+      </Card>
+    </PageWrapper>
+  );
+}
+
+function getTimings({ startDate, endDate }) {
+  // XXX: Handle case where start date isn't equal to end date
+  //      and add some nice things like checking if this is today
+
+  return [
+    format(startDate, "ddd Do MMM, h:mma"),
+    "-",
+    format(endDate, "h:mma")
+  ].join(" ");
 }
 
 EventPage.propTypes = {
-  url: PropTypes.object.isRequired
+  event: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
+
+export default compose(
+  withData({
+    query: graphql`
+      query event_Query($id: ID!) {
+        event(id: $id) {
+          title
+          address
+          organiser
+          startDate
+          endDate
+          longDescription
+          postcode
+        }
+      }
+    `
+  }),
+  withStyles(styles)
+)(EventPage);
