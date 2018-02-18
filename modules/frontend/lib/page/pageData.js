94fe6729ee9244eadc04d1ce8b8d04ae357f02e3
Chris Devereux <devereux.chris@gmail.com>
2018-02-18 22:25:19 +0000

    refactor(frontend): Use recompose to wrap HOCs

--- modules/frontend/lib/page/pageData.js
+++ modules/frontend/lib/page/pageData.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { fetchQuery } from "react-relay";
+import { wrapDisplayName } from "recompose";
 import PropTypes from "prop-types";
 import RelayContextProvider from "relay-context-provider";
 import createEnvironment from "accelerate-api-interface";
@@ -9,64 +10,69 @@ import { BACKEND_URL } from "../config";
  * Wrap a top-level page component with a NextJS getInitialProps() method
  * to fetch the data required for the page before starting to render it.
  */
-export default ({ query }) => ComposedComponent =>
-  class extends React.Component {
-    static displayName = `WithData(${ComposedComponent.displayName ||
-      ComposedComponent.name})`;
+export default function pageData({ query }) {
+  if (!query) {
+    return WrappedComponent => WrappedComponent;
+  }
 
-    static async getInitialProps(ctx) {
-      const inheritedProps = ComposedComponent.getInitialProps
-        ? await ComposedComponent.getInitialProps(ctx)
-        : {};
+  return WrappedComponent =>
+    class extends React.Component {
+      static displayName = wrapDisplayName(WrappedComponent, "pageData");
 
-      // Get the appropriate Relay environment
-      const environment = createEnvironment({
-        backendUrl: BACKEND_URL
+      static async getInitialProps(ctx) {
+        const inheritedProps = WrappedComponent.getInitialProps
+          ? await WrappedComponent.getInitialProps(ctx)
+          : {};
+
+        // Get the appropriate Relay environment
+        const environment = createEnvironment({
+          backendUrl: BACKEND_URL
+        });
+
+        // Provide page query params and any other initial props fetched for
+        // the page as variables to the page's Relay query.
+        const variables = {
+          ...ctx.query,
+          ...inheritedProps
+        };
+
+        // Fetch the page data and get the store contents in JSON form for
+        // rehydration
+        const queryProps = await fetchQuery(environment, query, variables);
+        const queryRecords = environment
+          .getStore()
+          .getSource()
+          .toJSON();
+
+        return {
+          ...inheritedProps,
+          ...queryProps,
+          queryRecords
+        };
+      }
+
+      static propTypes = {
+        queryRecords: PropTypes.objectOf(PropTypes.any).isRequired
+      };
+
+      get variables() {
+        return {};
+      }
+
+      environment = createEnvironment({
+        backendUrl: BACKEND_URL,
+        records: this.props.queryRecords
       });
 
-      // Provide page query params and any other initial props fetched for
-      // the page as variables to the page's Relay query.
-      const variables = {
-        ...ctx.query,
-        ...inheritedProps
-      };
-
-      // Fetch the page data and get the store contents in JSON form for
-      // rehydration
-      const queryProps = await fetchQuery(environment, query, variables);
-      const queryRecords = environment
-        .getStore()
-        .getSource()
-        .toJSON();
-
-      return {
-        ...inheritedProps,
-        ...queryProps,
-        queryRecords
-      };
-    }
-
-    static propTypes = {
-      queryRecords: PropTypes.objectOf(PropTypes.any).isRequired
+      render() {
+        return (
+          <RelayContextProvider
+            environment={this.environment}
+            variables={this.variables}
+          >
+            <WrappedComponent {...this.props} />
+          </RelayContextProvider>
+        );
+      }
     };
-
-    get variables() {
-      return {};
-    }
-
-    environment = createEnvironment({
-      backendUrl: BACKEND_URL,
-      records: this.props.queryRecords
-    });
-
-    render() {
-      return (
-        <RelayContextProvider
-          environment={this.environment}
-          variables={this.variables}
-        >
-          <ComposedComponent {...this.props} />
-        </RelayContextProvider>
-      );
-    }
-  };
+}
